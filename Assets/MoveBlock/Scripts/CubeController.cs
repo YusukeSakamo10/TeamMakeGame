220a02b6ddab41ec9a8eaab6a9269667a3e95f44
ShimuraShota <k021g1445@g.neec.ac.jp>
2022-10-21 11:35:59 +0900

    Move Block 修正　矢印キーでX軸とZ軸方向に移動可能 ＆　リファクタリング済み

--- Assets/MoveBlock/Scripts/CubeController.cs
+++ Assets/MoveBlock/Scripts/CubeController.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    Vector3 MoveX = new Vector3(1, 0, 0);
-    Vector3 MoveY = new Vector3(0, 1, 0);
-
     Vector3 target;
     Vector3 prevPos;
 
-    float step = 2f;
+    [SerializeField] float step = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -32,33 +29,15 @@ public class CubeController : MonoBehaviour
     // ‡A “ü—Í‚É‰‚¶‚ÄˆÚ“®Œã‚ÌˆÊ’u‚ğZo
     void SetTargetPosition()
     {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
 
         prevPos = target;
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            target = transform.position + MoveX;
-
-            return;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            target = transform.position - MoveX;
-
-            return;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            target = transform.position + MoveY;
-
-            return;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            target = transform.position - MoveY;
-
-            return;
-        }
+        
+        target = transform.position +dir;
+      
     }
     // ‡B –Ú“I’n‚ÖˆÚ“®‚·‚é
     void Move()

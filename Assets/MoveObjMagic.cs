d2e7eab4412b558a0f8b2e1226e81abb5de7f952
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2022-10-21 16:11:21 +0900

    カメラ

--- Assets/MoveObjMagic.cs
+++ Assets/MoveObjMagic.cs
@@ -11,9 +11,7 @@ public class MoveObjMagic : MonoBehaviour
     {
 
     }
-	public GameObject _bullet;
 
-    Vector3 _camera2Mouse;
 	void Update()
 	{
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -21,14 +19,7 @@ public class MoveObjMagic : MonoBehaviour
 		Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.green);
 		if (Physics.Raycast(ray, out hit, 100f, _layerMask))
 		{
-
-			Debug.Log(ray);
-			Debug.Log(hit);
-
-
-			//Destroy(hit.collider.gameObject);
 			Gimmick g = hit.collider.gameObject.GetComponent<Gimmick>();
-			Debug.Log(g.transform);
 			if (g)
             {
 				g.ChangeColor();

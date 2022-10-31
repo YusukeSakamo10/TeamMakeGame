d7b4dee6e4672323bd1dc35a3e57a9eec21892c2
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2022-10-31 14:41:01 +0900

    Tミノ途中

--- Assets/BlockRay.cs
+++ Assets/BlockRay.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class BlockRay : MonoBehaviour
 {
+    [SerializeField] float RayLange = 1;
+
     void Update()
     {
         Vector3 rayPosition = new(transform.position.x, transform.position.y, transform.position.z);
-        
 
-        Debug.DrawRay(rayPosition, transform.right, Color.red);
-        Debug.DrawRay(rayPosition, -transform.right, Color.red);
-        Debug.DrawRay(rayPosition, transform.forward, Color.red);
-        Debug.DrawRay(rayPosition, -transform.forward, Color.red);
+        Debug.DrawRay(rayPosition, transform.right * RayLange, Color.red);
+        Debug.DrawRay(rayPosition, -transform.right * RayLange, Color.red);
+        Debug.DrawRay(rayPosition, transform.forward * RayLange, Color.red);
+        Debug.DrawRay(rayPosition, -transform.forward * RayLange, Color.red);
 
     }
 }

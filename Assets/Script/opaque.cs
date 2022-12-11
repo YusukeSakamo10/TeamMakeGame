241ae97e04dd3d39efe2eab2a56e2f5f51aa6144
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2022-12-11 09:33:53 +0900

    アウトライン完成アウトラインのマテリアルあとで変更可

--- Assets/Script/opaque.cs
+++ Assets/Script/opaque.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class opaque : MonoBehaviour
 {
-    //透明状態にする時にどれくらいの alpha にするか指定する
-    [SerializeField] float targetAlpha = 0.6f;
-    //元の状態のα
-    float m_opaque = 1f;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Block")
@@ -39,19 +34,6 @@ public class opaque : MonoBehaviour
             Gimmick g = other.gameObject.GetComponent<Gimmick>();
             Renderer r = other.gameObject.GetComponent<Renderer>();
             r.material = g.originalMat;
-        }
-    }
-
-    // alphaを変更する関数
-    void ChangeAlpha(Renderer renderer, float targetAlpha)
-    {
-        if (renderer)
-        {
-            Material m = renderer.material;
-            Color c = m.color;
-            //αを書き換えてマテリアルに戻す
-            c.a = targetAlpha;
-            m.color = c;
         }
     }
 }

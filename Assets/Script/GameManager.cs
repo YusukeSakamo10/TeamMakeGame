03618706405f1475f7ffb5a1ae9e44eeb064611a
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2023-01-28 01:29:06 +0900

    カメラ途中

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;    // usingを忘れないで
 using UnityEngine.UI;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,7 +15,6 @@ public class GameManager : MonoBehaviour
     [Tooltip("buttonを押した時に呼び出す処理")]
     [SerializeField] UnityEvent _onShoot = null;
 
-
     //[Tooltip("残り移動回数の初期値")]
     //[SerializeField] int _initialMove = 5;
 
@@ -23,6 +23,11 @@ public class GameManager : MonoBehaviour
     /// <summary>残り移動回数を表示するための GameObject</summary>
     GameObject _moveObject;
 
+    static bool[] _clearFlag = {false,false,false,false,false };
+
+    [SerializeField] CinemachineVirtualCamera[] _vCam = {null, null, null, null, null };
+    int cameraTime = 60;
+    int cameraMaxTime = 60;
 
     public int MoveCountValue
     {
@@ -38,17 +43,31 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        moveCount =0;
+        moveCount = 0;
 
         _moveObject = GameObject.Find("MoveText");
         _moveText = _moveObject.GetComponent<Text>();
        _moveText.text = string.Format("{0:000}", maxMoveCount);
     }
 
-
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        for (int i = 0; i < 5; i++)
+        {
+            if (_clearFlag[i])
+            {
+                cameraTime--;
+                _vCam[i].Priority = 100;
+                if (cameraTime < 0)
+                {
+                    _vCam[i].Priority = 0;
+                    _clearFlag[i] = false;
+                    cameraTime = cameraMaxTime;
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             _onJump.Invoke();
         }
@@ -79,5 +98,12 @@ public class GameManager : MonoBehaviour
     public void ChangeScene(string nextScene)  // 新しくメソッド追加
     {
         SceneManager.LoadScene(nextScene);
+    }
+
+    public void ClearFlag(string nextScene)
+    {
+        if (nextScene == "turtorial") { _clearFlag[0] = true; }
+        if (nextScene == "stage1") { _clearFlag[1] = true; }
+        if (nextScene == "stage2") { _clearFlag[2] = true; }
     }
 }

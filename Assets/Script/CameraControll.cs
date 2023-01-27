24e059d489b457587de1b9a3eef8be3f07c21bca
HAYASHITAKAYUKI <k021g1446@g.neec.ac.jp>
2023-01-27 11:58:25 +0900

    カメラ上向けるように

--- Assets/Script/CameraControll.cs
+++ Assets/Script/CameraControll.cs
@@ -5,20 +5,18 @@ using Cinemachine;
 
 public class CameraControll : MonoBehaviour
 {
-    private CinemachineVirtualCamera _vCam;
-
     void Start()
     {
-        //_vCam = GetComponent<CinemachineVirtualCamera>();
         CinemachineCore.GetInputAxis = GetAxisCustom;
     }
 
     public float GetAxisCustom(string axisName)
     {
         //水平方向に移動しながらマウスでも回転するとカクつくので制御している
-        if (Input.GetKey(KeyCode.D)){ return 0; }
-        else if(Input.GetKey(KeyCode.A)){ return 0; }
-        else
+        if (Input.GetKey(KeyCode.D)) { return 0; }
+        else if (Input.GetKey(KeyCode.A)) { return 0; }
+        //マウスのX軸方向の移動取得
+        else if (axisName == "Mouse X")
         {
             if (Input.GetMouseButton(1))
             {
@@ -26,5 +24,15 @@ public class CameraControll : MonoBehaviour
             }
             return 0;
         }
+        //マウスのY軸方向の取得
+        else if (axisName == "Mouse Y")
+        {
+            if (Input.GetMouseButton(1))
+            {
+                return UnityEngine.Input.GetAxis("Mouse Y");
+            }
+            return 0;
+        }
+        return 0;
     }
 }
